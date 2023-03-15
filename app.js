daf6ff3f6f4bfbf6400e8843c3a895d5e5022da7
dodgersden <116809310+dodgersden@users.noreply.github.com>
2023-03-16 02:05:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Define your API routes
+app.get('/hello', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
